35067a8beb8740707e722a2616e93fb6c0d47a0a
zhxymh <13552710076@126.com>
2019-06-22 12:10:38 +0800

    change network get blocks timeout

--- src/AElf.OS.Network.Grpc/GrpcPeer.cs
+++ src/AElf.OS.Network.Grpc/GrpcPeer.cs
@@ -23,7 +23,7 @@ namespace AElf.OS.Network.Grpc
         private const int AnnouncementTimeout = 300;
         private const int BlockRequestTimeout = 300;
         private const int TransactionBroadcastTimeout = 300;
-        private const int BlocksRequestTimeout = 500;
+        private const int BlocksRequestTimeout = 3000;
         
         private enum MetricNames
         {
